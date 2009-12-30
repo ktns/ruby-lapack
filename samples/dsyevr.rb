6efc3d7cbfb1907c884033f5cfb590805e75b391
Seiya Nishizawa <seiya@gfd-dennou.org>
2009-12-30 00:27:14 +0000

    * add sample

---
+++ samples/dsyevr.rb
@@ -0,0 +1,22 @@
+require "numru/lapack"
+
+jobz = "V"
+range = "I"
+uplo = "U"
+a = NArray[[1,1,0], [1,2,1], [0,1,1]]
+#a = NArray[[1,0], [2,3]]
+vl = vu = 0 # not be used in this example
+il = 1
+iu = 3
+#iu = 2
+abstol = 0.0
+lwork = 78
+liwork = 30
+
+m, w, z, isuppz, work, iwork, info, a =
+  NumRu::Lapack.dsyevr(jobz, range, uplo,
+                       a, vl, vu, il, iu, abstol, lwork, liwork)
+require "pp"
+pp [m, w, z, isuppz, work, iwork, info, a]
+
+
