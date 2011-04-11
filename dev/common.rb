7403847cae27b9109e0b559211253871707cc501
Seiya Nishizawa <seiya@gfd-dennou.org>
2011-04-11 16:08:01 +0900

    ilaenv_ is removed to variable list

--- dev/common.rb
+++ dev/common.rb
@@ -1,6 +1,6 @@
 def get_vars(dim)
   ary = Array.new
-  dim.gsub(/MAX\(/,",").gsub(/MIN\(/,",").gsub(/log\(/,",").gsub(/abs\(/,",").gsub(/sqrt\(/,",").gsub(/pow\(/,",").gsub(/LG\(/,",").gsub(/lsame_\(\&([^,]+),[^)]+\)/,'\1').gsub(/ilatrans_\([^)]+\)/,",").gsub(/[\(\)\+\-\*\/:\?=\&\|]+/,",").split(",").each{|d|
+  dim.gsub(/MAX\(/,",").gsub(/MIN\(/,",").gsub(/log\(/,",").gsub(/abs\(/,",").gsub(/sqrt\(/,",").gsub(/pow\(/,",").gsub(/LG\(/,",").gsub(/lsame_\(\&([^,]+),[^)]+\)/,'\1').gsub(/ilatrans_\([^)]+\)/,",").gsub(/ilaenv_\(([^,]+),[^,]+/,'\1').gsub(/[\(\)\+\-\*\/:\?=\&\|]+/,",").split(",").each{|d|
     d.strip!
     next if (d == "") || (/^\d+(\.\d+)?$/ =~ d) || /^\"[^\"]+\"$/ =~ d || d=="int" || d=="double"
     ary.push d
