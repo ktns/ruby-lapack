cfc35eae869008cf051d5a911ae7cfbf5f1740a8
Seiya Nishizawa <seiya@gfd-dennou.org>
2011-04-12 09:35:22 +0900

    add module LapackTest
    add modules: assert_narray, get_int, get_rc

---
+++ tests/lapack_test.rb
@@ -0,0 +1,43 @@
+require "test/unit"
+require "numru/lapack"
+
+module LapackTest
+
+  I = Complex::I
+
+  def assert_narray(expected, actual, delta=nil, message="")
+    unless delta
+      case actual.typecode
+      when NArray::SFLOAT, NArray::SCOMPLEX
+        delta = 1.0e-5
+      when NArray::DFLOAT, NArray::DCOMPLEX
+        delta = 1.0e-13
+      when NArray::INT, NArray::LINT
+        delta = 0
+      else
+        raise "typecode is invalid"
+      end
+    end
+    if message.empty?
+      message = <<EOF
+<#{expected.inspect}>
+and
+<#{actual.inspect}>
+expected to have maximan differnce <#{(expected-actual).abs.max}> within
+<#{delta}>.
+EOF
+    end
+    assert (expected - actual).abs.max <= delta, message
+  end
+
+  def get_int(x)
+    x = x.real if x.respond_to?(:real)
+    x.to_i
+  end
+
+  def get_rc(x)
+    /\A[sd]/ =~ x ? :r : :c
+  end
+  module_function :get_rc
+
+end
